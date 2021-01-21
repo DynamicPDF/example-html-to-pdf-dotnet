2027f7d313cff69b7dd3742ee99f24fbc8aa1af8
Andrew Cochran <andrew@dynamicpdf.com>
2021-01-21 15:29:21 -0500

    Added example for 3 products.

---
+++ ConverterExample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ceTe.DynamicPDF.Conversion;
+
+namespace example_html_to_pdf_dotnet
+{
+    class ConverterExample
+    {
+        public static void Convert()
+        {
+            HtmlConversionOptions options = new HtmlConversionOptions(false);
+            HtmlConverter htmlConverter = new HtmlConverter(new Uri(@"http://www.google.com"), options);
+            htmlConverter.Convert("output.pdf");
+        }
+    }
+}
---
+++ HtmlConverterExample.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using ceTe.DynamicPDF.HtmlConverter;
+
+namespace example_html_to_pdf_dotnet
+{
+    class HtmlConverterExample
+    {
+        public static void Convert()
+        {
+            ConversionOptions options = new ConversionOptions(PageSize.A4, PageOrientation.Portrait, 50.0f);
+            // Set Metadata for the PDF
+            options.Author = "Myself";
+            options.Title = "My Webpage";
+            // Set Header and Footer text
+            options.Header = "<div style=\"text-align:center;display:inline-block;width:100%;font-size:12px;\">" +
+                "<span class=\"date\"></span></div>";
+            options.Footer = "<div style=\"text-align:center;display:inline-block;width:100%;font-size:12px;\">" +
+                "Page <span class=\"pageNumber\"></span> of <span class=\"totalPages\"></span></div>";
+            // Convert with Options          
+            Converter.Convert(new Uri("https://www.google.com"), "output.pdf", options);
+        }
+    }
+}
